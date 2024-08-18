059242f8e009c31dcf1bb7da570ca0f5f10123e6
akbarIT-prog <akbarpratenta5@gmail.com>
2024-08-18 19:30:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multiple-text", {
+
+        strings:
+
+        ["3 func Fact Tentang Saya","Saya Tidak Suka Foto","Tidak Suka Bawang","Masih Nungguin Cewek Dari 2021",],
+        typeSpeed: 250,
+        typeSpeed: 250,
+        backDelay: 1000,
+        loop: true
+});
